48d6f423a18dc46a40cf54983e5169887a14afe8
Muhammad Ahmed Cheema <54610255+mcheemaa@users.noreply.github.com>
2020-11-30 15:29:51 -0500

    First Commit - Final

---
+++ blockchain.py
@@ -0,0 +1,199 @@
+import time
+import hashlib
+import urllib.parse
+from urllib.parse import urlparse
+import json
+import random
+
+
+class Block:
+    def __init__(self, index, timestamp, transactions, previous_hash, nonce=0, portnumber = 5000):
+        self.index = index
+        self.timestamp = timestamp
+        self.transactions = transactions
+        self.previous_hash = previous_hash
+        self.nonce = nonce
+        self.portnumber = portnumber
+
+    def get_port(self):
+        return self.portnumber
+
+
+    def calc_hash(self):
+        # Get SHA256 hash of block
+
+        block_string = json.dumps(self.__dict__, sort_keys=True)
+        #print("\nBlock String : ", block_string)
+        raw_hash = hashlib.sha256(block_string.encode())
+        hex_hash = raw_hash.hexdigest()
+        return hex_hash
+
+        
+    def calc_proof(self, difficulty):
+        # Simple proof of work algorithm - hash must begin with # of zeros defined by difficulty property
+
+        hash = self.calc_hash()
+        #print("Hash : ",  hash)
+
+
+        start = time.perf_counter()
+        print (start)
+        while not hash.startswith('0' * difficulty):
+            self.nonce += 1
+            hash = self.calc_hash()
+            #print("Hash Inside : ",  hash)
+            if ((time.perf_counter() - start) >= 10):
+                hash = 404
+                return hash
+
+        return hash
+
+
+class Blockchain:
+    def __init__(self):
+        self.chain = []
+        self.pending_transactions = []
+        self.nodes = set()
+        self.create_genesis_block()
+
+
+    @property
+    def last_block(self):
+        # Get last block in the chain
+
+        return self.chain[-1]
+
+
+    @property
+    def difficulty(self):
+        # Complexity of proof of work algorithm
+
+        return 4
+
+
+    def create_genesis_block(self):
+        # Create the first block in the chain
+
+        genesis_block = Block(
+            index = 0,
+            timestamp = 0,
+            transactions = [],
+            previous_hash = '0'
+        )
+
+        self.chain.append(genesis_block)
+
+
+    def new_transaction(self, sender, recipient, amount):
+        # Add transaction to the list
+
+        transaction = {
+            'sender': sender,
+            'recipient': recipient,
+            'amount': amount
+        }
+
+        self.pending_transactions.append(transaction)
+
+        return len(self.chain)
+
+
+    def mine(self, portN):
+        # Create new block with pending transactions
+
+        if not self.pending_transactions:
+            return False
+
+        block = Block(
+            index = len(self.chain),
+            timestamp = time.time(),
+            transactions = self.pending_transactions,
+            previous_hash = self.last_block.calc_hash(),
+            portnumber = portN  
+        )
+
+        added = self.add_block(block)
+
+        if added:
+            self.pending_transactions = []
+
+            return block
+        else:
+            return False
+
+
+    def add_block(self, block):
+        # Forge block to chain if valid
+        porti = block.get_port()
+        lastblock = self.last_block
+        lastport = self.last_block.get_port()
+
+        diff = self.difficulty
+
+        if (porti == lastport):
+            print("Warning Too Many Mining Requests - Difficulty Will Be Increased")
+            diff = self.difficulty + 2
+            proof = block.calc_proof(diff)
+        else:
+            proof = block.calc_proof(self.difficulty)
+
+        if (block.calc_hash == 404):
+            return False
+        if block.previous_hash != self.last_block.calc_hash():
+            return False
+
+        if not self.validate_proof(block, proof, diff):
+            return False
+
+        self.chain.append(block)
+
+        return True
+
+
+    def validate_proof(self, block, proof, diff):
+        # Validate the hash of a block 
+
+        return (block.calc_hash().startswith('0' * diff) and proof == block.calc_proof(diff))
+
+
+    def validate_chain(self, chain):
+        # Ensure validity of block hashes to ensure the chain hasn't been tampered with
+
+        for i in range (2, len(chain)):
+            current_block = chain[i]
+            previous_block = chain[i-1]
+          
+            if current_block.previous_hash != previous_block.calc_hash():
+                return False
+
+            if not current_block.calc_hash().startswith('0' * self.difficulty):
+                return False
+
+        return True
+
+
+    def hack_chain(self, block_index, transaction_id):
+        # Illegally change a transaction in a block in the chain to invalidate the chain (testing purposes)
+
+        block = self.chain[block_index]
+        transactions = []
+
+        for i in range(0, len(block.transactions)-1):
+            if i == transaction_id:
+                sender = random.randint(0, len(self.nodes) - 1)
+                recipient = random.randint(0, len(self.nodes) - 1)
+                amount = random.randint(1, 100)
+                new_transaction = {
+                    'sender' : sender,
+                    'recipient' : recipient,
+                    'amount' : amount
+                }
+                transactions.append(new_transaction)
+            else:
+                transactions.append(block.transactions[i])
+
+        block.transactions = transactions
+        self.chain[block_index] = block
+
+        return block_index
+
---
+++ network.py
@@ -0,0 +1,221 @@
+import blockchain
+import flask 
+from flask import Flask, request, session
+from flask_script import Manager, Server, Command, Option
+from flask_session import Session
+from uuid import uuid4
+import json
+import requests
+import sys
+import random
+
+
+my_blockchain = blockchain.Blockchain()
+
+app = Flask(__name__)
+app.debug = True
+SESSION_TYPE = 'filesystem'
+app.config.from_object(__name__)
+Session(app)
+manager = Manager(app)
+
+portNum = 1
+
+
+def echo_nodes():
+    for node in range(5000, int(session['node_id'])):
+        my_blockchain.nodes.add(node)
+
+        url = 'http://127.0.0.1:' + str(node) + '/nodes/new'
+        data = {'node': session['node_id']}
+        headers = {'Content-type': 'application/json'}
+        r = requests.post(url=url, json=data, headers=headers)
+
+
+@manager.command
+def runserver(port):
+    portNum = port
+    session['node_id'] = port
+    echo_nodes()
+    app.run(port=int(port))
+
+
+@app.route('/transactions', methods=['GET'])
+def get_transactions():
+    if not my_blockchain.pending_transactions:
+        return 'No pending transactions'
+        
+    return json.dumps(my_blockchain.pending_transactions)
+
+
+@app.route('/transactions/new', methods=['POST'])
+def new_transactions():
+    data = request.get_json()
+
+    # Check that required fields are in post response
+    required = ['sender', 'recipient', 'amount']
+    if not all(k in data for k in required):
+        return 'Error: Missing transaction data', 400
+        
+    block_index = my_blockchain.new_transaction(data['sender'], data['recipient'], data['amount'])
+        
+    for node in my_blockchain.nodes:
+        url = 'http://127.0.0.1:' + str(node) + '/transactions/add'
+        headers = {'Content-type': 'application/json'}
+        response = requests.post(url=url, json=data, headers=headers)
+
+    return f'Transaction will be added to block {block_index}', 201
+
+
+#Internal use
+@app.route('/transactions/add', methods=['POST'])
+def add_transactions():
+    data = request.get_json()
+
+    block_index = my_blockchain.new_transaction(data['sender'],  data['recipient'], data['amount'])
+
+    return 'Transaction added to pending transactions'
+
+
+@app.route('/chain', methods=['GET'])
+def get_chain():
+    chain = []
+    for block in my_blockchain.chain:
+        chain.append(block.__dict__)
+
+    return json.dumps(chain)
+
+
+@app.route('/chain/hack', methods=['GET'])
+def hack_chain():
+    block_index = random.randint(1, len(my_blockchain.chain)-1)
+    transaction_id = random.randint(0, len(my_blockchain.chain[block_index].transactions)-1)
+    my_blockchain.hack_chain(block_index, transaction_id)
+
+    return f'Block {block_index} changed'
+
+
+@app.route('/mine/port', methods=['POST'])
+def mine():
+    data = request.get_json()
+    portNumber = data['port']
+    print(portNumber)
+    block = my_blockchain.mine(data['port'])
+
+
+    if not block:
+        return 'Nik Cheem Blockchain - Error Connection Timed Out (Exceeded Time Limit)'
+
+    # Reward for mining
+    my_blockchain.new_transaction(
+        sender = 0,
+        recipient = session.get('node_id'),
+        amount = 1
+    )
+
+    for node in my_blockchain.nodes:
+        url = 'http://127.0.0.1:' + str(node) + '/blocks/new'
+        data = json.dumps(block.__dict__, sort_keys=True)
+        headers = {'Content-type': 'application/json'}
+        r = requests.post(url=url, json=data, headers=headers)
+
+    return f'Block {block.index} is mined'
+
+#Internal use
+@app.route('/blocks/new', methods=['POST'])
+def new_block():
+    data = json.loads(request.get_json())
+
+    block = blockchain.Block(
+        index = data['index'],
+        timestamp = data['timestamp'],
+        transactions = data['transactions'],
+        previous_hash = data['previous_hash'],
+        nonce = data['nonce']             
+    )
+
+    added = my_blockchain.add_block(block)
+
+    if not added:
+        return 'Block was discarded by the node', 400
+
+    return 'Block added to chain', 201
+
+
+@app.route('/nodes', methods=['GET'])
+def get_peers():
+    if not my_blockchain.nodes:
+        return 'Node does not have any peers'
+
+    nodes = []
+    for node in my_blockchain.nodes:
+        nodes.append(node)
+
+    return json.dumps(nodes)
+
+
+#Internal use
+@app.route('/nodes/new', methods=['POST']) 
+def new_node():
+    data = request.get_json()
+
+    node = data['node']
+    if node is None:
+        return 'Error: Please supply a valid node', 400
+
+    my_blockchain.nodes.add(node)
+
+    response = {
+        'message': 'New node added',
+        'total_nodes': len(my_blockchain.nodes)
+    }
+
+    return response, 201
+
+
+@app.route('/nodes/resolve', methods=['GET'])
+def consensus():
+    # Replace chain with longest valid chain in network
+
+    replaced = False
+    longest_chain = my_blockchain.chain
+    chain_len = len(my_blockchain.chain)
+
+    for node in my_blockchain.nodes:
+        chain = []
+
+        # Create blockchain instance from json string returned by node
+        url = 'http://127.0.0.1:' + str(node) + '/chain'
+        response = requests.get(url)
+        data = response.json()
+        
+        for i in range(0, len(data)):
+            block = blockchain.Block(
+                index = data[i]['index'],
+                timestamp = data[i]['timestamp'],
+                transactions = data[i]['transactions'],
+                previous_hash = data[i]['previous_hash'],
+                nonce = data[i]['nonce']             
+            )
+
+            chain.append(block)
+
+        if my_blockchain.validate_chain(longest_chain):
+            if my_blockchain.validate_chain(chain) and len(chain) > chain_len:
+                replaced = True
+                chain_len = len(chain)
+                longest_chain = chain
+        else:
+            replaced = True
+            chain_len = len(chain)
+            longest_chain = chain
+           
+    if replaced:
+        my_blockchain.chain = longest_chain
+        return 'Our chain was replaced'
+
+    return 'Our chain is authoritative'
+
+
+if __name__ == '__main__':
+    manager.run()
